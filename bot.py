c3615f064fd7e5d0d73529656c47391c6390b14f
ajsya <52182942+ajsya@users.noreply.github.com>
2021-08-18 09:05:41 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,14 @@ from discord.ext import commands
 from mcstatus import MinecraftServer
 from datetime import datetime
 import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bot = commands.Bot(command_prefix='pog ')
-server_ip = "" # ENTER DESIRED SERVER IP HERE
+
+server_ip = os.getenv("SERVERIP")
 server = MinecraftServer.lookup(server_ip) #poggerchair IP: 95.142.162.123:25565
 
 async def request():
@@ -14,7 +19,7 @@ async def request():
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         status = server.status()
-        channel = bot.get_channel() # ENTER YOUR CHANNEL ID HERE
+        channel = bot.get_channel(841751991088971827) # REPLACE THE CHANNEL ID WITH YOURS
         await channel.edit(topic=str("{0}/20 players online | Last Edited at {1} EST".format(status.players.online, current_time)), reason="Automatic Edit: Player Count Changed")
         print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
         await asyncio.sleep(300)
@@ -39,5 +44,5 @@ async def playerCount(ctx):
         await ctx.send("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
         print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))       
 
-TOKEN = "" # ENTER YOUR BOT TOKEN HERE
+TOKEN = os.getenv("TOKEN")
 bot.run(TOKEN)
